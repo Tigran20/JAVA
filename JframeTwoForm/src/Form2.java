412b0903d8ec19a267f16e7362379528515a8e9b
Tigran20 <Podliza122@bk.ru>
2017-03-21 18:18:34 +0300

    qwe

--- JframeTwoForm/src/Form2.java
+++ JframeTwoForm/src/Form2.java
@@ -8,16 +8,10 @@ public class Form2 {
     private JButton switchBtn2;
     private JLabel userData;
     private JPanel secondPanel;
+    private JProgressBar progressBar;
 
     public Form2 (){
         secondPanel.setLayout(new BoxLayout(secondPanel, BoxLayout.X_AXIS));
-        secondPanel.add((Box.createRigidArea(new Dimension(100, 0))));
-        secondPanel.add(userData);
-        secondPanel.add((Box.createRigidArea(new Dimension(6, 0))));
-        secondPanel.add(textField);
-        secondPanel.add((Box.createRigidArea(new Dimension(15, 0))));
-        secondPanel.add(switchBtn2);
-        secondPanel.add((Box.createRigidArea(new Dimension(15, 0))));
     }
 
     public JPanel getRootPanel() {
