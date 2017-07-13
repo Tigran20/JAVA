0a35b8f410117af4c475fb83dbf5b987ace43997
Tigran20 <Podliza122@bk.ru>
2017-07-13 21:15:58 +0300

    1

---
+++ SongListTest/src/Album.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Album {
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<>();
+    }
+
+    public boolean addSong(String title, double duration){
+        if (findSong(title) == null) {
+            this.songs.add(new Song(title,duration));
+            return true;
+        }
+        return false;
+    }
+
+    private Song findSong (String title){
+        for(Song chekedSong : this.songs){
+            if(chekedSong.getTitle().equals(title)){
+                return chekedSong;
+            }
+        }
+        return null;
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playList){
+        int index = trackNumber - 1;
+        if((index > -1) && (index <= this.songs.size())){
+            playList.add(this.songs.get(index));
+            return true;
+        }
+        System.out.println("В этом альбоме нет песни с номером " + trackNumber);
+        return false;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playList){
+        Song checkedSong = findSong(title);
+        if(checkedSong != null){
+            playList.add(checkedSong);
+            return true;
+        }
+        System.out.println("Песня " + title + " не в этом альбоме");
+        return false;
+    }
+}
---
+++ SongListTest/src/Main.java
@@ -0,0 +1,234 @@
+import java.util.*;
+
+public class Main {
+
+    private static ArrayList<Album> albums = new ArrayList<>();
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        Album album = new Album("Hybrid Theory", "Linkin Park");
+
+        album.addSong("Papercut", 3.05);
+        album.addSong("One Step Closer", 2.36);
+        album.addSong("With You", 3.23);
+        album.addSong("Points of Authority", 3.20);
+        album.addSong("Crawling", 3.29);
+        album.addSong("Runaway", 3.03);
+        album.addSong("By Myself", 3.10);
+        album.addSong("In The End", 3.36);
+        albums.add(album);
+
+
+        Album anotherAlbum = new Album("Back in Black", "AC/DC");
+
+        anotherAlbum.addSong("Hells Bells", 5.12);
+        anotherAlbum.addSong("Shoot to Thrill", 5.17);
+        anotherAlbum.addSong("What Do You Do For Money Honey", 3.35);
+        anotherAlbum.addSong("Givin The Dog A Bone", 3.32);
+        anotherAlbum.addSong("Let Me Put My Love into You", 4.15);
+        anotherAlbum.addSong("Back in Black", 4.15);
+        albums.add(anotherAlbum);
+
+
+        LinkedList<Song> playList = new LinkedList<>();
+        albums.get(0).addToPlayList(1, playList);
+        albums.get(0).addToPlayList(2, playList);
+        albums.get(0).addToPlayList(3, playList);
+        albums.get(0).addToPlayList(4, playList);
+        albums.get(0).addToPlayList(5, playList);
+        albums.get(0).addToPlayList(6, playList);
+        albums.get(0).addToPlayList(7, playList);
+        albums.get(0).addToPlayList(8, playList);
+
+
+        LinkedList<Song> playList2 = new LinkedList<>();
+        albums.get(1).addToPlayList(1, playList2);
+        albums.get(1).addToPlayList(2, playList2);
+        albums.get(1).addToPlayList(3, playList2);
+        albums.get(1).addToPlayList(4, playList2);
+        albums.get(1).addToPlayList(5, playList2);
+        albums.get(1).addToPlayList(6, playList2);
+
+        playAlbum(playList, playList2);
+
+    }
+
+    private static void playAlbum(LinkedList<Song> playList, LinkedList<Song> playList2) {
+        boolean quit = false;
+        if (playList.size() == 0)
+        {
+            System.out.println("Нет плейлистов");
+            return;
+        }
+        else
+        {
+            printAlbumMenu();
+        }
+
+        while (!quit) {
+            int chooseAlbum = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (chooseAlbum) {
+                case 0:
+                    System.out.println("Плейлист закончен");
+                    quit = true;
+                    break;
+                case 1:
+                    play(playList);
+                    quit = true;
+                    break;
+                case 2:
+                    play(playList2);
+                    quit = true;
+                    break;
+                case 3:
+                    printAlbumMenu();
+                    break;
+            }
+        }
+    }
+
+
+    private static void play(LinkedList<Song> playList) {
+        boolean quit = false;
+        boolean forward = true;
+
+        ListIterator<Song> listIterator = playList.listIterator();
+        if (playList.size() == 0)
+        {
+            System.out.println("Нет песен в плейлисте");
+            return;
+        }
+        else
+        {
+            System.out.println("Сейчас играет: " + listIterator.next().toString());
+            printMenu();
+        }
+
+        while (!quit) {
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (action){
+                case 0:
+                    System.out.println("Плейлист закончен");
+                    quit = true;
+                    break;
+                case 1:
+                    if(!forward)
+                    {
+                        if(listIterator.hasNext())
+                        {
+                            listIterator.next();
+                        }
+                        forward = true;
+                    }
+                    if(listIterator.hasNext())
+                    {
+                        System.out.println("Сейчас играет: " + listIterator.next().toString());
+                    }
+                    else
+                    {
+                        System.out.println("Мы в конце плейлиста");
+                        forward = false;
+                    }
+                    break;
+                case 2:
+                    if(forward)
+                    {
+                        if (listIterator.hasPrevious())
+                        {
+                            listIterator.previous();
+                        }
+                        forward = false;
+                    }
+                    if(listIterator.hasPrevious())
+                    {
+                        System.out.println("Сейчас играет: " + listIterator.previous().toString());
+                    }
+                    else
+                    {
+                        System.out.println("Мы в начале плейлиста");
+                        forward = true;
+                    }
+                    break;
+                case 3:
+                    if(forward)
+                    {
+                        if(listIterator.hasPrevious()) {
+                            System.out.println("Сейчас играет: " + listIterator.previous().toString());
+                            forward = false;
+                        }
+                        else
+                        {
+                            System.out.println("Мы в начале плейлиста");
+                        }
+                    }
+                    else
+                    {
+                        if (listIterator.hasNext())
+                        {
+                            System.out.println("Сейчас играет: " + listIterator.next().toString());
+                            forward = true;
+                        }
+                        else
+                        {
+                            System.out.println("Мы в конце плейлиста");
+                        }
+                    }
+                    break;
+                case 4:
+                    printList(playList);
+                    break;
+                case 5:
+                    printMenu();
+                    break;
+                case 6:
+                    if(playList.size() > 0)
+                    {
+                        listIterator.remove();
+                        if(listIterator.hasNext())
+                        {
+                            System.out.println("Сейчас играет: " + listIterator.next());
+                        }
+                        else if (listIterator.hasPrevious())
+                        {
+                            System.out.println("Сейчас играет: " + listIterator.previous());
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+
+    private static void printMenu() {
+        System.out.println("Доступные действия: \nнажмите" );
+        System.out.println("0 - для выхода \n" +
+                "1 - чтобы воспроизвести следующую песню\n" +
+                "2 - чтобы воспроизвести предыдущую песню\n" +
+                "3 - для повтора текущей песни\n" +
+                "4 - список песен в плейлисте\n" +
+                "5 - вывести список действий\n" +
+                "6 - удалить текущую песню из плейлиста\n"
+                );
+    }
+
+    private static void printAlbumMenu() {
+        System.out.println("Доступные действия: \nнажмите" );
+        System.out.println("0 - для выхода \n" +
+                "1 - для перехода в альбом: Linkin Park\n" +
+                "2 - для перехода в альбом: AC/DC\n" +
+                "3 - вывести список действий\n");
+    }
+
+    private static void printList(LinkedList<Song> playList){
+        Iterator<Song> iterator = playList.iterator();
+        System.out.println("===========================");
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next().toString());
+        }
+        System.out.println("===========================");
+    }
+}
